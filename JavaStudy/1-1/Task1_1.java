e7ffa9c5f29b03c8d5900915dc1135cced8abe6d
ktyhym <107557480+ktyhym@users.noreply.github.com>
2022-07-03 23:02:56 +0900

    Create Task1_1.java

---
+++ JavaStudy/1-1/Task1_1.java
@@ -0,0 +1,20 @@
+public class Task1_1 {
+    public static void main(String[] args) {
+        
+        int hogeInt = 50;
+
+        String hogeString = テスト;
+
+        doubleクラスの hogedouble という名前の変数を宣言、値「0.2」で初期化
+
+        int hogeInt = 100;
+
+        String hogeString = 合格;
+
+        System.out.println(hogeInt);
+
+        hogeStringの値を表示
+
+        System.out.println(hogeDouble);
+    }
+}
