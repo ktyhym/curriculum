6e8a71f48b2a3be22ca53e5a3a61261b6aeb9247
ktyhym <107557480+ktyhym@users.noreply.github.com>
2022-07-05 00:02:29 +0900

    Create Task1_2.java

---
+++ JavaStudy/1-2/Task1_2.java
@@ -0,0 +1,19 @@
+public class Task1_2{
+    
+    public static void main(String[] args) {
+        String[] country = {"JAPAN", "AMERICA", "KOREA"};
+        System.out.println(country.length);
+
+        String[] strArray = {"りんご","もも","ぶどう"};
+        System.out.println(strArray[1]);
+
+        10, 20, 30, 40, 50を要素の値（初期値）とする配列intArrayを定義
+        
+        int[] intArray = { 10, 20, 30, 40, 50 };
+
+        配列2番目と配列3番目の合計を表示
+        System.out.println(intArray[1] + intArray[4]);
+        
+    }
+
+}
