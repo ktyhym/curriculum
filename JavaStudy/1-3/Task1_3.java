7b83f456930982b79c953d26ffe102e72c51de45
ktyhym <107557480+ktyhym@users.noreply.github.com>
2022-07-13 22:36:31 +0900

    Create Task1_3.java

---
+++ JavaStudy/1-3/Task1_3.java
@@ -0,0 +1,28 @@
+public class Task1_3 {
+
+    public static void main(String[] args) {
+        int plus = 0;
+        plus = 5 + 10;
+        System.out.println(plus);
+
+        int minus = 0;
+        minus = 20 - 7;
+        System.out.println(minus);
+
+        10と2を乗算した結果を変数multiplyに代入している
+        int multiply = 0;
+        multiply = 10 * 2;
+        System.out.println(multiply);
+
+
+        int remi = 20;
+        remi = remi %= 6;
+        System.out.println(remi);
+
+        変数townに有楽町を代入。変数lineに線を代入
+        String town = "有楽町";
+        String line = "線";
+        String train = town + line;
+        System.out.println(train);
+    }
+}
